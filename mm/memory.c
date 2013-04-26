d2e0197183edd989131458539cedf029a1706001
Janson Kang <temasek71@gmail.com>
2013-04-26 18:28:40 +0800

    Linux 3.0.75

--- mm/memory.c
+++ mm/memory.c
@@ -2430,8 +2430,12 @@ EXPORT_SYMBOL(remap_pfn_range);
  * NOTE! Some drivers might want to tweak vma->vm_page_prot first to get
  * whatever write-combining details or similar.
  */
+<<<<<<< HEAD
 int vm_iomap_memory(struct vm_area_struct *vma, phys_addr_t start,
 							unsigned long len)
+=======
+int vm_iomap_memory(struct vm_area_struct *vma, phys_addr_t start, unsigned long len)
+>>>>>>> 8c5fb68... Linux 3.0.75
 {
 	unsigned long vm_len, pfn, pages;
 
@@ -2461,8 +2465,12 @@ int vm_iomap_memory(struct vm_area_struct *vma, phys_addr_t start,
 		return -EINVAL;
 
 	/* Ok, let it rip */
+<<<<<<< HEAD
 	return io_remap_pfn_range(vma, vma->vm_start, pfn, vm_len,
 							vma->vm_page_prot);
+=======
+	return io_remap_pfn_range(vma, vma->vm_start, pfn, vm_len, vma->vm_page_prot);
+>>>>>>> 8c5fb68... Linux 3.0.75
 }
 EXPORT_SYMBOL(vm_iomap_memory);
 
