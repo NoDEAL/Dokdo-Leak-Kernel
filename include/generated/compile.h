e45eb588b2d29b4f485b3a6cb2c640ba2e6d5dd9
GwonHyeok <kh4975@naver.com>
2013-10-22 23:10:51 +0900

    add Ramfs&I9300 Defconfig

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 5 */
+/* This file is auto generated, version 1 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#5 SMP PREEMPT Mon Oct 21 23:48:58 KST 2013"
+#define UTS_VERSION "#1 SMP PREEMPT Tue Oct 22 23:05:50 KST 2013"
 #define LINUX_COMPILE_BY "root"
 #define LINUX_COMPILE_HOST "hyeok-desktop"
 #define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "3.0.31-Hyeok_Kernel*_*-allfix"
+#define UTS_RELEASE "3.0.31-Hyeok_Kernel*_*-allfix-gca6c8c9-dirty"
